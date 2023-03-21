3c8259011f8ef07f16efd3a3122e2ac924b47ce1
ubermanu <manu@paca.happy-dev.fr>
2023-03-21 14:23:41 +0100

    rename base layer and remove some unused keys

--- keymap.c
+++ keymap.c
@@ -2,13 +2,13 @@
 #include "keymap_extras/keymap_dvorak.h"
 
 
-#define _QWERTY 0
+#define _BASE 0
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  BASE = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
@@ -16,7 +16,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
+  [_BASE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -36,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, DV_DQUO, DV_LABK, DV_RABK, _______, _______,                            _______, _______, _______, _______, _______, KC_INS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, KC_LCBR, KC_LBRC,                            KC_RBRC, _______, _______, _______, KC_PLUS, KC_PIPE,
+     _______, _______, _______, _______, DV_QUES, KC_PSLS,                            KC_RBRC, _______, _______, _______, _______, KC_PIPE,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LPRN,          KC_RPRN, KC_RCBR, _______, _______, _______, KC_MINS, KC_RCTL,
+     KC_LCTL, KC_COLN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LPRN,          KC_RPRN, KC_RCBR, _______, _______, _______, _______, KC_RCTL,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                    _______, _______, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -75,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
+    case BASE:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+        set_single_persistent_default_layer(_BASE);
       }
       return false;
       break;
