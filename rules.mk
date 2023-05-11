d216990009b83142920f4718e4d8fcc764594760
ubermanu <manu@paca.happy-dev.fr>
2023-05-11 12:46:05 +0200

    feat: add accents with unicode

--- rules.mk
+++ rules.mk
@@ -1,1 +1,6 @@
 LAYOUT=dvorak
+UNICODEMAP_ENABLE = yes
+#UNICODE_ENABLE = yes
+EXTRAFLAGS+=-flto
+CONSOLE_ENABLE   = no   # Console for debug
+COMMAND_ENABLE   = no   # Commands for debug and configuration
